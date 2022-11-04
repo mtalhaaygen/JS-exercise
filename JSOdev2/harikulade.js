114688a9c84d65463bce243f8262e2afdca0dd32
M. Talha Aygen <talha.aygen18@hotmail.com>
2022-11-04 17:17:08 +0300

    js_todolisit_update

--- JSOdev2/harikulade.js
+++ JSOdev2/harikulade.js
@@ -1,23 +1,60 @@
-// Yazacağınız javascript kodu içerisinde element eklemeyi sağlayan bir fonksiyon, element silmeyi sağlayan bir fonksiyon, yapıldı işaretlenmesini sağlayan bir fonksiyon olması gerekiyor.
-
 let ulDOM = document.querySelector("#list")
+let i = 0;
+while (localStorage.getItem(i) != null) 
+{
+    // bu while ile her sayfa yenilendiğinde localStroge deki veriler geri yükleniyor
+    const liDOM = document.createElement("li");
+    liDOM.innerHTML = localStorage.getItem(i);
+    // liDOM.setAttribute("onclick", "textdeco()");
+    ulDOM.append(liDOM);
+    i++;
+}
 function newElement(){
-    const liDOM = document.createElement("li")
-    let veri = document.querySelector("#task").value
-    liDOM.innerHTML = veri
-    console.log(liDOM)
-    ulDOM.append(liDOM)
-
+    let veri;
+    const liDOM = document.createElement("li");
+    if (veri = document.querySelector("#task").value) // sadece space eklenmesine engel ol
+    {
+        liDOM.innerHTML = veri;
+        // liDOM.setAttribute("onclick", "textdeco()");
+        ulDOM.append(liDOM);
+        localStorage.setItem(i, veri);
+        i++;
+        console.log("oluşturuldu toast");
+    }
+    else
+    {
+        console.log("boş ekleme uyarı toast");
+        //toast mesaj uyarı 
+        //Listeye boş ekleme yapamazsınız!
+        // id="liveToast2" 
+    }
 }
 
-function DeleteElement(){
-    let ulDOM = document.querySelector("#list")
-    ulDOM.innerHTML = ulDOM.innerHTML.slice(0,1)
-
+function DeleteAll(){
+    if(ulDOM.innerHTML != "")
+    {
+        // hepsi silinecek tamam or iptal if ile kontrol et
+        console.log("hepsi silinsin mi toast");
+        let ulDOM = document.querySelector("#list")
+        ulDOM.innerHTML = null;
+        i = 0;
+        localStorage.clear();
+    }
+    else
+    {
+        console.log("liste zaten boş");
+    }
+}
+function textdeco()
+{
+    item.setAttribute("style", "text-deca:red;");
 }
 
-let lilerim = document.querySelectorAll("li")
-lilerim.onclick = function(){
-    console.log("isaretle fonksiyonu")
-};
-
+// element.setAttribute("style", "background-color:red;");
+// let lilerim = document.querySelectorAll("li");
+// lilerim.forEach(textdeco);
+// ulDOM.onclick = function(){ // bu sayfa yenileme fonksiyonuna ihtiyaç duymamın sebebi 
+//     console.log("isaretle fonksiyonu");
+    
+// //     window.location.reload();
+// };
